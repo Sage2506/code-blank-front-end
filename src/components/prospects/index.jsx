a701ac4f1cf845ebc1e36e814027c2279f5c057c
German Eduardo Salazar Aranda <german@agricompra.com>
2024-06-10 13:56:56 -0700

    uploading files

--- src/components/prospects/index.jsx
+++ src/components/prospects/index.jsx
@@ -1,16 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { getProspects,postProspect, putProspect } from '../../services/prospect_request';
+import { getProspects, postProspect, putProspect } from '../../services/prospect_request';
+import { Button } from 'react-bootstrap';
 import ProspectTable from './table';
 import { default as Pagination } from '../common/pagination';
 import ProspectModalForm from './modalForm';
-
+import axios from "axios";
 export class ProspectsIndex extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      prospect: { name: ""},
-      showModal: false
+      prospect: {},
+      showModal: false,
+      attachments: []
     }
   }
 
@@ -18,50 +20,80 @@ export class ProspectsIndex extends Component {
     this.props.getProspects();
   }
 
-  componentDidUpdate(){
-    if(!!this.props.prospect.id){
-      let { pagination, getProspects } = this.props
-      getProspects({page: pagination.currentPage});
+  componentDidUpdate() {
+    if (!!this.props.prospect.name) {
+      if (!this.props.prospect.file && this.state.attachments && this.state.attachments.length > 0) {
+        this.uploadFiles()
+      }
     }
   }
 
   selectedProspect = prospect => {
-    this.setState({prospect, showModal: true})
+    this.setState({ prospect, showModal: true })
+  }
+
+  openFormModal = () => {
+    this.setState({ showModal: true, prospect: {} });
   }
 
   handleInputChange = e => {
-    this.setState({
-      prospect: {
-        ...this.state.prospect,
-        [e.target.id] : e.target.value
-      }
-    })
+    if (e.target.id === "attachment") {
+      const file = e.target.files[0];
+      const fileName = e.target.files[0].name;
+      this.setState({
+        prospect: { ...this.state.prospect, attachment: file, attachment_name: fileName }
+      })
+    } else {
+      this.setState({
+        prospect: { ...this.state.prospect, [e.target.id]: e.target.value }
+      })
+    }
   }
 
   close = () => {
-    this.setState({showModal: false})
+    this.setState({ showModal: false, attachments: [] })
   }
 
-  submit = () => {
-    this.setState({showModal:false})
-    if(this.state.prospect.id){
+  submit = (attachments) => {
+    this.setState({ showModal: false, attachments })
+    if (this.state.prospect.id) {
       this.props.putProspect(this.state.prospect.id, this.state.prospect);
     } else {
       this.props.postProspect(this.state.prospect);
     }
-
   }
+
+  uploadFiles = () => {
+    let promises = [];
+    this.state.attachments.forEach(attachment => {
+      if (attachment.attachment_name.length > 0) {
+        promises.push(this.createFilePromise(attachment))
+      }
+    });
+    Promise.all(promises).then(results => {
+      this.setState({ prospect: {}, showModal: false, attachments: [] })
+      this.props.getProspects({ page: 1 });
+    })
+  }
+
+  createFilePromise = attachment => {
+    const data = new FormData();
+    data.append('file', attachment.attachment, attachment.attachment_name);
+    data.append('prospect_id', this.props.prospect.id);
+    return axios.post('http://localhost:4200/files', data);
+  }
+
   render() {
     let { pagination, getProspects } = this.props;
     return (
       <div>
         <ProspectModalForm
-        showModal={this.state.showModal}
-        prospect={this.state.prospect}
-        handleInputChange={this.handleInputChange}
-        submit={this.submit}
-        close={this.close}/>
-
+          showModal={this.state.showModal}
+          prospect={this.state.prospect}
+          handleInputChange={this.handleInputChange}
+          submit={this.submit}
+          close={this.close} />
+        <Button onClick={() => this.openFormModal()}>Nuevo prospecto</Button>
         <ProspectTable selectedProspect={this.selectedProspect} />
         <Pagination pagination={pagination} paginationRequest={getProspects} />
       </div>
@@ -79,10 +111,10 @@ const mapDispatchToProps = dispatch => {
     getProspects: params => {
       dispatch(getProspects(params))
     },
-    putProspect : (id,data) => {
-      dispatch(putProspect({id, data}))
+    putProspect: (id, data) => {
+      dispatch(putProspect({ id, data }))
     },
-    postProspect : data => {
+    postProspect: data => {
       dispatch(postProspect(data))
     }
   }
--- src/components/prospects/modalForm.jsx
+++ src/components/prospects/modalForm.jsx
@@ -1,96 +1,237 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Modal, Form, Button } from "react-bootstrap";
+import { Modal, Form, Button, Row, Col } from "react-bootstrap";
 
 export class ProspectModalForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      validated: false,
+      attachments: []
+    }
+  }
+
+  addFileField = () => {
+    this.setState({ attachments: [...this.state.attachments, { attachment: "", attachment_name: "" }] })
+  }
+
+  removeFileField = (idx) => {
+    let { attachments } = this.state
+    attachments.splice(idx, 1)
+    this.setState({ attachments })
+  }
+
+  submit = () => {
+    this.setState({ validated: true });
+    let { name, middle_name, last_name, street, ext_number, neighborhood, zip_code, phone_number, rfc } = this.props.prospect;
+    if (
+      name?.length > 0 &&
+      middle_name?.length > 0 &&
+      street?.length > 0 &&
+      ext_number?.length > 0 &&
+      neighborhood?.length > 0 &&
+      zip_code?.length > 0 &&
+      phone_number?.length > 0 &&
+      rfc?.length > 0) {
+      this.props.submit([...this.state.attachments])
+      this.setState({ validated: false, attachments: [] })
+    } else {
+      console.log("something is missing")
+    }
+  }
+
+  handleFileInputChange = (e, idx) => {
+    const file = e.target.files[0];
+    const fileName = e.target.files[0].name;
+    let { attachments } = this.state
+    attachments[idx] = { attachment_name: fileName, attachment: file }
+    this.setState({ attachments });
+  }
+
+  close = () => {
+    this.setState({ validated: false })
+    this.props.close()
+  }
 
   render() {
-    let { showModal, prospect, submit, close, handleInputChange } = this.props;
-    let { name, middle_name, last_name, street, ext_number, neighborhood, zip_code, phone_number, rfc } = prospect;
+    let { validated } = this.state
+    let { showModal, prospect, handleInputChange } = this.props;
+    let { name, middle_name, last_name, street, ext_number, neighborhood, zip_code, phone_number, rfc, files } = prospect;
     return (
-      <Modal show={showModal} onHide={close} >
+      <Modal show={showModal} onHide={() => this.close()} >
         <Modal.Header closeButton>
           <Modal.Title>Formulario</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <Form>
-            <Form.Group controlId="name">
-              <Form.Control
-                type="text"
-                value={name}
-                placeholder="Describe a dish name"
-                onChange={handleInputChange}
-                isValid={name !== ""} />
-            </Form.Group>
-            <Form.Group controlId="middle_name">
-              <Form.Control
-                type="text"
-                value={middle_name}
-                placeholder="Describe a dish middle_name"
-                onChange={handleInputChange}
-                isValid={middle_name !== ""} />
-            </Form.Group>
-            <Form.Group controlId="last_name">
-              <Form.Control
-                type="text"
-                value={last_name}
-                placeholder="Describe a dish last_name"
-                onChange={handleInputChange}
-                isValid={last_name !== ""} />
-            </Form.Group>
-            <Form.Group controlId="street">
-              <Form.Control
-                type="text"
-                value={street}
-                placeholder="Describe a dish street"
-                onChange={handleInputChange}
-                isValid={street !== ""} />
-            </Form.Group>
-            <Form.Group controlId="ext_number">
-              <Form.Control
-                type="text"
-                value={ext_number}
-                placeholder="Describe a dish ext_number"
-                onChange={handleInputChange}
-                isValid={ext_number !== ""} />
-            </Form.Group>
-            <Form.Group controlId="neighborhood">
-              <Form.Control
-                type="text"
-                value={neighborhood}
-                placeholder="Describe a dish neighborhood"
-                onChange={handleInputChange}
-                isValid={neighborhood !== ""} />
-            </Form.Group>
-            <Form.Group controlId="zip_code">
-              <Form.Control
-                type="text"
-                value={zip_code}
-                placeholder="Describe a dish zip_code"
-                onChange={handleInputChange}
-                isValid={zip_code !== ""} />
-            </Form.Group>
-            <Form.Group controlId="phone_number">
-              <Form.Control
-                type="text"
-                value={phone_number}
-                placeholder="Describe a dish phone_number"
-                onChange={handleInputChange}
-                isValid={phone_number !== ""} />
-            </Form.Group>
-            <Form.Group controlId="rfc">
-              <Form.Control
-                type="text"
-                value={rfc}
-                placeholder="Describe a dish rfc"
-                onChange={handleInputChange}
-                isValid={rfc !== ""} />
-            </Form.Group>
+          {!prospect.id &&
+            <Form onSubmit={this.submit} validated={validated}>
+              <Form.Group controlId="name">
+                <Form.Control
+                  type="text"
+                  required
+                  value={name || ""}
+                  placeholder="Nombre"
+                  maxLength={150}
+                  onChange={handleInputChange}
+                  isValid={name?.length > 0} />
+                <Form.Control.Feedback type="invalid">
+                  Este campo es obligatorio
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="middle_name">
+                <Form.Control
+                  type="text"
+                  required
+                  value={middle_name || ""}
+                  placeholder="Apellido materno"
+                  onChange={handleInputChange}
+                  maxLength={150}
+                  isValid={middle_name?.length > 0} />
+                <Form.Control.Feedback type="invalid">
+                  Este campo es obligatorio
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="last_name">
+                <Form.Control
+                  type="text"
+                  value={last_name || ""}
+                  placeholder="Apellido paterno"
+                  maxLength={150}
+                  onChange={handleInputChange} />
+              </Form.Group>
+              <Form.Group controlId="street">
+                <Form.Control
+                  type="text"
+                  required
+                  value={street || ""}
+                  placeholder="Calle"
+                  onChange={handleInputChange}
+                  maxLength={150}
+                  isValid={street?.length > 0} />
+                <Form.Control.Feedback type="invalid">
+                  Este campo es obligatorio
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="ext_number">
+                <Form.Control
+                  type="text"
+                  required
+                  value={ext_number || ""}
+                  placeholder="Numero"
+                  onChange={handleInputChange}
+                  maxLength={150}
+                  isValid={ext_number?.length > 0} />
+                <Form.Control.Feedback type="invalid">
+                  Este campo es obligatorio
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="neighborhood">
+                <Form.Control
+                  type="text"
+                  required
+                  value={neighborhood || ""}
+                  placeholder="Colonia"
+                  onChange={handleInputChange}
+                  maxLength={150}
+                  isValid={neighborhood?.length > 0} />
+                <Form.Control.Feedback type="invalid">
+                  Este campo es obligatorio
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="zip_code">
+                <Form.Control
+                  type="text"
+                  required
+                  value={zip_code || ""}
+                  placeholder="Codigo postal"
+                  onChange={handleInputChange}
+                  maxLength={10}
+                  isValid={zip_code?.length > 0} />
+                <Form.Control.Feedback type="invalid">
+                  Este campo es obligatorio
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="phone_number">
+                <Form.Control
+                  type="text"
+                  required
+                  value={phone_number || ""}
+                  placeholder="Telefono"
+                  onChange={handleInputChange}
+                  maxLength={10}
+                  minLength={7}
+                  isValid={phone_number?.length > 0} />
+                <Form.Control.Feedback type="invalid">
+                  Este campo es obligatorio
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group controlId="rfc">
+                <Form.Control
+                  type="text"
+                  required
+                  value={rfc || ""}
+                  placeholder="RFC"
+                  onChange={handleInputChange}
+                  maxLength={12}
+                  minLength={12} />
+                <Form.Control.Feedback type="invalid">
+                  Este campo es obligatorio
+                </Form.Control.Feedback>
 
-          </Form>
+              </Form.Group>
+              <Button onClick={() => this.addFileField()}>Documento (+)</Button>
+              {this.state.attachments.map((attachment, index) =>
+                <Row key={`fileInput${index}`}>
+                  {!attachment.attachment &&
+                    <Form.Group as={Col} xs={8} controlId="attachment" className="mb-3">
+                      <Form.Control type="file" onChange={(event) => this.handleFileInputChange(event, index)} />
+                    </Form.Group>
+                  }
+                  {!!attachment.attachment_name &&
+                    <Form.Group as={Col} xs={8} controlId="attachment" className="mb-3">
+                      <Form.Control value={attachment.attachment_name} readOnly className="mb-3" />
+                    </Form.Group>
+                  }
+                  <Button as={Col} xs={4} onClick={() => this.removeFileField(index)}> remover</Button>
+                </Row>
+              )}
+            </Form>
+          }
+          {!!prospect.id &&
+            <Form>
+              <p>Nombre: {name}</p>
+              <p>Apellido paterno: {middle_name}</p>
+              <p>Apellido materno {last_name}</p>
+              <p>Calle: {street}</p>
+              <p>Numero: {ext_number}</p>
+              <p>Colonia: {neighborhood}</p>
+              <p>Codigo postal: {zip_code}</p>
+              <p>Telefono: {phone_number}</p>
+              <p>RFC: {rfc}</p>
+
+              {!!files && files.map(file =>
+                <p key={`file_${file.id}`}>
+                  <a href={`http://localhost:4200/uploads/${file.name}`} target="_blank" >
+                    {file.name}
+                  </a>
+                </p>
+              )}
+              <Form.Select>
+                <option value="0">
+                  Enviado
+                </option>
+                <option value="1">
+                  Autorizado
+                </option>
+                <option value="2">
+                  Rechazado
+                </option>
+              </Form.Select>
+            </Form>
+          }
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => submit()}>
+          <Button variant="secondary" onClick={() => this.submit()}>
             Accept
           </Button>
         </Modal.Footer>
